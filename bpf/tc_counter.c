b5e61b70c0b8a1b01db0b03cd378e7929536f008
Michael (Mike) Williams <34947926+mpwusr@users.noreply.github.com>
2026-02-01 18:01:37 -0500

    add two sections for egress and ingress

--- bpf/tc_counter.c
+++ bpf/tc_counter.c
@@ -4,19 +4,26 @@
 
 struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
-  __uint(max_entries, 1);
+  __uint(max_entries, 2);
   __type(key, __u32);
   __type(value, __u64);
 } counter SEC(".maps");
 
-SEC("tc")
-int tc_ingress(struct __sk_buff *skb) {
-  __u32 key = 0;
+static __always_inline void inc(__u32 key) {
   __u64 *val = bpf_map_lookup_elem(&counter, &key);
-  if (val) {
-    __sync_fetch_and_add(val, 1);
-  }
-  return 0; // allow packet
+  if (val) __sync_fetch_and_add(val, 1);
+}
+
+SEC("tc/ingress")
+int tc_ingress(struct __sk_buff *skb) {
+  inc(0);
+  return 0;
+}
+
+SEC("tc/egress")
+int tc_egress(struct __sk_buff *skb) {
+  inc(1);
+  return 0;
 }
 
 char LICENSE[] SEC("license") = "GPL";
